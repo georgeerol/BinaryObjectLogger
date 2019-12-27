597b90924fe2d4f94b5b0330dbc4e7208065ce86
George Fouche <fouliex@gmail.com>
2019-12-26 23:58:07 -0800

    Add ClassValidation tests

---
+++ src/test/java/binarylogger/ClassValidationTest.java
@@ -0,0 +1,43 @@
+package binarylogger;
+
+import loggable.NotLoggable;
+import loggable.SystemLog;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by George Fouche on 12/26/19.
+ */
+public class ClassValidationTest {
+
+    @Test
+    public void testAValidClass() throws ClassNotFoundException {
+        ClassValidation classValidation = new ClassValidation(SystemLog.class.getCanonicalName());
+        assertTrue(classValidation.isValid());
+    }
+
+
+    @Test(expected = ClassNotFoundException.class)
+    public void testAInvalidClass() throws ClassNotFoundException {
+        ClassValidation classValidation = new ClassValidation("TheClass");
+        classValidation.isValid();
+    }
+
+    @Test
+    public void testAnInstanceOfBinaryLoggable() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        ClassValidation classValidation = new ClassValidation(SystemLog.class.getCanonicalName());
+        assertTrue(classValidation.isInstanceOfBinaryLoggable());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAnInstanceOfNotBinaryLoggable() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        ClassValidation classValidation = new ClassValidation(NotLoggable.class.getCanonicalName());
+        classValidation.isInstanceOfBinaryLoggable();
+    }
+
+
+}
